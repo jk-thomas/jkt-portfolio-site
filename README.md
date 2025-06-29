bf0a4a5285afb10030dff7645e3bfa96ec421475
Joshua Thomas <josht2999@gmail.com>
2025-06-29 01:17:48 -0500

    v0.3

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # jkt-portfolio-site
+
+## Versions
+v1 - HTML / CSS / Javascript / JQuery
+v2 - Tailwind
+v3 - NodeJS / Express
+v4 - MongoDB
+v5 - Nginx / Cloudflare / Docker
+v6 - React
+v7 - Admin
---
+++ projects.js
@@ -0,0 +1,12 @@
+// create new branch for new front end design
+// buttons with semi circle sides
+// themes:
+// 	k background
+// 	t text colors
+
+// chatgpt:
+// portfolio site system:
+// ctrl f:
+// fetch
+// json
+
