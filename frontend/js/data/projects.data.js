d3a9bd66c291d1edb60d695b9c888d3286dd5875
Joshua Thomas <josht2999@gmail.com>
2026-03-02 01:40:35 -0600

    [docs] project card info updated

--- frontend/js/data/projects.data.js
+++ frontend/js/data/projects.data.js
@@ -1,156 +1,163 @@
 export const projects = [
     {
         title: "Portfolio Site",
-        description: "multiplayer",
+        description: "This site, containerized and deployed on a self-hosted Raspberry Pi using Docker and reverse proxy routing ",
         tech: ["HTML/CSS", "Javascript", "Docker", "Nginx"],
-        github: "#",
+        github: "https://github.com/jk-thomas/jkt-portfolio-site",
         live: ""
     },
     {
         title: "ValetConnect",
-        description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker", "Socket.io"],
+        description: "Real-time valet service management system, instant messaging between staff and customers",
+        tech: ["React", "NodeJS", "PostgreSQL", "Docker", "Firebase", "AWS"],
         github: "",
         live: ""
     },
     {
         title: "Mission EMR",
-        description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Team developed healthcare application suite",
+        tech: ["Flutter", "Dart", "Hive"],
+        github: "",
+        live: ""
     },
     {
         title: "Silent Space Locator",
-        description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Team developed mobile app to find nearest location with least noise",
+        tech: ["Java", "Android Studio"],
+        github: "",
+        live: ""
     },
     {
         title: "UOMe",
-        description: "multiplayer",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Lite web app for tracking shared purchases in a group and auto calculates payments",
+        tech: ["React", "NodeJS", "Express", "SQLite"],
+        github: "https://github.com/jk-thomas/UOMe",
+        live: "https://uome-gamma.vercel.app/"
     },
     {
         title: "BlogSystem",
-        description: "multiplayer",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "A blogging website made using the classic LAMP stack",
+        tech: ["Linux", "Apache", "MySQL", "PHP"],
+        github: "https://github.com/jk-thomas/BlogSystem",
+        live: ""
     },
     {
         title: "Database Systems",
-        description: "multiplayer",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Library management application using PostgreSQL",
+        tech: ["C++", "PostgreSQL"],
+        github: "",
+        live: ""
     },
     {
         title: "Artificial Intelligence",
         description: "Search, Multi-agent Search, Reinforcement Learning, Machine Learning",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        tech: ["Python"],
+        github: "https://github.com/jk-thomas/UIC/tree/main/CS411",
+        live: ""
     },
     {
         title: "Stay Safe Project",
-        description: "multiplayer",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Webapp that shows real-time crime alerts nearby",
+        tech: ["Javascript"],
+        github: "",
+        live: ""
     },
     {
-        title: "Threaded HTTP Server",
-        description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        title: "Concurrent HTTP Server",
+        description: "HTTP server implemented with threads",
+        tech: ["C++"],
+        github: "https://github.com/jk-thomas/UIC/tree/main/CS361",
+        live: ""
     },
     {
         title: "Networked Poker",
-        description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Networked version of 3 Card Poker",
+        tech: ["Java", "JavaFX", "Java Sockets"],
+        github: "https://github.com/jk-thomas/UIC/tree/main/CS342",
+        live: ""
     },
     {
         title: "Networked Chatroom",
         description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        tech: ["Java", "FXML"],
+        github: "https://github.com/jk-thomas/UIC/tree/main/CS342",
+        live: ""
     },
     {
         title: "Keno",
-        description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Lottery game",
+        tech: ["Java", "JavaFX"],
+        github: "https://github.com/jk-thomas/UIC/tree/main/CS342",
+        live: ""
     },
     {
         title: "Openstreet Maps",
-        description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Navigation program using openstreetmap",
+        tech: ["C++"],
+        github: "https://github.com/jk-thomas/UIC/tree/main/CS251",
+        live: ""
     },
     {
         title: "File Compression",
-        description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "File compression program using Huffman algorithm",
+        tech: ["C++"],
+        github: "https://github.com/jk-thomas/UIC/tree/main/CS251",
+        live: ""
     },
     {
         title: "Data Analysis",
-        description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Cancer data analysis and prediction program",
+        tech: ["Jupyter", "Python", "NumPy", "Pandas"],
+        github: "",
+        live: ""
     },
     {
         title: "BitTorrent",
-        description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Simulated clients using BitTorrent protocol",
+        tech: ["Python"],
+        github: "https://github.com/jk-thomas/UIC/tree/main/CS407",
+        live: ""
+    },
+    {
+        title: "Database Applications",
+        description: "Python program to analyze/read/update database",
+        tech: ["Python", "SQLite"],
+        github: "https://github.com/jk-thomas/UIC/tree/main/CS341",
+        live: ""
     },
     {
         title: "RPS Multiplayer Game",
-        description: "Real-time multiplayer game using Socket.io",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Real-time multiplayer rock, paper, scissors deployed on LAN",
+        tech: ["NodeJS", "Express", "Socket.io", "Docker"],
+        github: "https://github.com/jk-thomas/odin-rock-paper-scissors/tree/multiplayer",
+        live: "rps.local:3000"
     },
     {
         title: "Candy Crush",
-        description: "multiplayer",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "JS version of the popular game Candy Crush",
+        tech: ["Javascript"],
+        github: "",
+        live: ""
     },
     {
         title: "FinDash",
-        description: "multiplayer",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Financial Dashboard made with NextJS",
+        tech: ["Next.js", "React", "Typescript", "NodeJS"],
+        github: "https://github.com/jk-thomas/FinDash",
+        live: ""
     },
     {
         title: "TheOdinProject",
-        description: "multiplayer",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Frontend projects",
+        tech: ["HTML/CSS", "Javascript", "React", "NodeJS"],
+        github: "",
+        live: ""
     },
     {
         title: "FullStackOpen",
-        description: "multiplayer",
-        tech: ["Node", "Express", "Docker"],
-        github: "#",
-        live: "#"
+        description: "Fullstack projects",
+        tech: ["React", "Javascript", "NodeJS", "Express", "GraphQL", "Typescript", "PostgreSQL"],
+        github: "https://github.com/jk-thomas/fullstackopen",
+        live: ""
     }
 ];
